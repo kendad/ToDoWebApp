142cf9443eae50900c530ae2ea347fb5199a10b9
Kaushik Iswary <kaushikiswary@gmail.com>
2021-09-13 23:54:17 +0530

    Add session storage using cookies

--- frontend/server.js
+++ frontend/server.js
@@ -2,17 +2,19 @@ const express = require('express')
 const axios = require('axios')
 const app = express()
 const path=require('path')
+const cookieParser=require('cookie-parser')
 
 app.set('view engine','ejs')
 app.use(express.static(path.join(__dirname,'views')))
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 
 api_url='http://localhost:8000/api/v1/'
 
 app.get('/', async (req,res)=>{
     axios.get(api_url,{
         headers:{
-            'Authorization':'Token d46c29adcec96b2370f1080d52f49c8c81105ac1'
+            'Authorization':'Token '+req.cookies['Token']
         }
     }).then((response)=>{
         //res.send(response.data)
@@ -29,7 +31,24 @@ app.get('/sample-form',(req,res)=>{
 })
 
 app.post('/sample-form',(req,res)=>{
-    res.render('html/sample_form.ejs',{'kela':req.body.simpleText})
+
+    axios.post(api_url+'auth/login/',
+    {
+        'username':req.body.username,
+        'password':req.body.password
+    },
+    {
+        headers:{
+            'Content-Type':'application/json'
+        }
+    }).then((response)=>{
+        //res.send(response.data)
+        res.render('html/sample_form',{'kela':response.data.key})
+    }).catch((err)=>{
+        console.log(err)
+    })
+
+    //res.render('html/sample_form.ejs',{'kela':req.body})
 })
 
 app.get('/sample-ajax',(req,res)=>{
