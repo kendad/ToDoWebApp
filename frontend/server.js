b84d9f59a0aa317b71e07cbb065844ad49d471b1
Kaushik Iswary <kaushikiswary@gmail.com>
2021-09-14 08:32:02 +0530

    first commit

--- frontend/server.js
+++ frontend/server.js
@@ -26,50 +26,5 @@ app.get('/', async (req,res)=>{
 
 })
 
-app.get('/sample-form',(req,res)=>{
-    res.render('html/sample_form.ejs',{'kela':''})
-})
-
-app.post('/sample-form',(req,res)=>{
-
-    axios.post(api_url+'auth/login/',
-    {
-        'username':req.body.username,
-        'password':req.body.password
-    },
-    {
-        headers:{
-            'Content-Type':'application/json'
-        }
-    }).then((response)=>{
-        //res.send(response.data)
-        res.render('html/sample_form',{'kela':response.data.key})
-    }).catch((err)=>{
-        console.log(err)
-    })
-
-    //res.render('html/sample_form.ejs',{'kela':req.body})
-})
-
-app.get('/sample-ajax',(req,res)=>{
-    res.render('html/sample-ajax.ejs')
-})
-
-app.get('/sample-ajax-kela', async (req,res)=>{
-    axios.get(api_url,{
-        headers:{
-            'Authorization':'Token d46c29adcec96b2370f1080d52f49c8c81105ac1'
-        }
-    }).then((response)=>{
-        //res.send(response.data)
-        res.render('html/kela_ajax.ejs',{'jsonData':response.data},(err,html)=>{
-            res.send(html)
-        })
-    }).catch((err)=>{
-        console.log(err)
-    })
-
-})
-
 
 app.listen(process.env.PORT||5000)
